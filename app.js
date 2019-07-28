4da7c977bdd26714170b769a90fc4f24665bbe2e
mubarak23 <mubarakaminu340@gmail.com>
2019-07-28 19:50:38 +0100

    create ui product and storage class

--- app.js
+++ app.js
@@ -9,4 +9,29 @@ const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productDom = document.querySelector('.products-center');
 
+//cart items
+let cart = [];
+//class that get the product from local data
+class Products{
+
+}
+
+
+//ui class 
+class UI{
+
+}
+
+//local storage
+class Storage{
+
+}
+
+
+
+document.addEventListener("DomContentLoaded", () =>{
+    const ui = new UI();
+    const products = new Products();
+    
+})
 
