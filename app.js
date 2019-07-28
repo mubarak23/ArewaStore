9c7eb6846dc44ce35c7bf0a9add18f124b4e7822
mubarak23 <mubarakaminu340@gmail.com>
2019-07-28 19:45:26 +0100

    create variable that query out classes

---
+++ app.js
@@ -0,0 +1,12 @@
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDom = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-over-lay');
+const cartItems = document.querySelector('.cart-items');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productDom = document.querySelector('.products-center');
+
+
