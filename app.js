c06acbc1a60106a259b520603cc1a47077f1ef0d
mubarak23 <mubarakaminu340@gmail.com>
2019-07-28 20:02:20 +0100

    add get product method that pull product data from local file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const clearCartBtn = document.querySelector('.clear-cart');
 const cartDom = document.querySelector('.cart');
 const cartOverlay = document.querySelector('.cart-over-lay');
 const cartItems = document.querySelector('.cart-items');
-const cartItems = document.querySelector('.cart-items');
+const cartItem = document.querySelector('.cart-item');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productDom = document.querySelector('.products-center');
@@ -13,7 +13,14 @@ const productDom = document.querySelector('.products-center');
 let cart = [];
 //class that get the product from local data
 class Products{
-
+   async getProducts(){
+     try {
+        let results = await fetch("products.json");
+        return results;
+     } catch (error) {
+         console.log(error);
+     }
+    }
 }
 
 
@@ -29,9 +36,12 @@ class Storage{
 
 
 
-document.addEventListener("DomContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () =>{
     const ui = new UI();
     const products = new Products();
-    
+
+  //get all products
+  products.getProducts().then(data => console.log(data))  
+
 })
 
